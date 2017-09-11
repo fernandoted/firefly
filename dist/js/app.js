2afe264a8080f55786ddd772eabb579e479c404d
fernandoted <fernandoted@icloud.com>
2017-09-11 13:16:35 -0300

    app.js adjust

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,3 +3,20 @@
 $(".heart.fa").click(function() {
 	$(this).toggleClass("fa-heart fa-heart-o");
 });
+
+function toplistTab(listName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = "block";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(listName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
